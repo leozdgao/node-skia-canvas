7f1d245790892d86a38e7538c537a3343d5fbcd0
leozdgao <gzdleo0816@outlook.com>
2021-11-16 14:07:26 +0800

    chore: github action

---
+++ scripts/centos7_build_action/build.js
@@ -0,0 +1,31 @@
+
+// RUN git clone --depth=1 https://github.com/leozdgao/node-skia-canvas.git 
+
+// WORKDIR /workspace/node-skia-canvas
+// RUN git submodule init && git submodule update
+
+const path = require('path')
+const { execSync } = require('child_process')
+
+const exec = (cmd, opts) => {
+  const { cwd = process.cwd(), stdio = 'inherit' } = opts || {}
+  return execSync(cmd, {
+    stdio,
+    cwd
+  })
+}
+
+const PREBUILD_UPLOAD_SECRET = process.argv[2] || process.env.PREBUILD_UPLOAD_SECRET || '';
+
+exec(`echo upload=${PREBUILD_UPLOAD_SECRET} >> ~/.prebuildrc`)
+
+exec('git clone --depth=1 https://github.com/leozdgao/node-skia-canvas.git')
+
+const workDirectory = path.join(process.cwd(), 'node-skia-canvas')
+
+exec('git submodule init && git submodule update', {
+    cwd: workDirectory
+})
+exec('npm run ci && npm test', {
+    cwd: workDirectory
+})
