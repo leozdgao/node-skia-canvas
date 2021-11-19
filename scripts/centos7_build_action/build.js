ff2ced99e5a5bb73f5fbbbe26c9d2e8c36ed4dbc
leozdgao <gzdleo0816@outlook.com>
2021-11-19 18:15:33 +0800

    chore: build action

--- scripts/centos7_build_action/build.js
+++ scripts/centos7_build_action/build.js
@@ -19,16 +19,6 @@ const PREBUILD_UPLOAD_SECRET = process.argv[2] || process.env.PREBUILD_UPLOAD_SE
 
 exec(`echo upload=${PREBUILD_UPLOAD_SECRET} >> ~/.prebuildrc`)
 
-// exec('git clone --depth=1 https://github.com/leozdgao/node-skia-canvas.git')
-
-const workDirectory = path.join(__dirname, '../node-skia-canvas')
-
-// exec('git submodule init && git submodule update', {
-//     cwd: workDirectory
-// })
-// exec('npm install', {
-//   cwd: workDirectory
-// })
 exec('npm run ci && npm test', {
-    cwd: workDirectory
+    cwd: '/work/node-skia-canvas'
 })
